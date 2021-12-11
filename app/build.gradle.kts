6fe97a2ac48e3aac1866e23962b08148ddd28863
William Swartzendruber <wswartzendruber@gmail.com>
2021-12-11 12:30:43 -0700

    Barebones Spring Boot application.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,28 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 repositories {
     mavenCentral()
 }
 
 plugins {
     kotlin("jvm").version("1.6.0")
+    id("org.springframework.boot").version("2.6.1")
+    id("io.spring.dependency-management").version("1.0.11.RELEASE")
     application
+}
+
+tasks {
+    withType<KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
 }
 
 dependencies {
     implementation(platform(kotlin("bom")))
     implementation(kotlin("stdlib-jdk8"))
+    implementation("org.springframework.boot:spring-boot-starter-web")
 }
 
 application {
--- app/src/main/kotlin/Application.kt
+++ app/src/main/kotlin/Application.kt
@@ -1,5 +1,11 @@
 package genericapi
 
-public fun main(args: Array<String>) {
-    println("Hello, world!")
+import org.springframework.boot.runApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public fun main(args: Array<String>): Unit {
+    runApplication<Application>(*args)
 }
+
+@SpringBootApplication
+public abstract class Application
